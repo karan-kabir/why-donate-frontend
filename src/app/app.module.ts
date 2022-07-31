f0e6bdf1ed164d9a1db7655f4199081bc04ecd22
Karan Kabir <karanschdev@gmail.com>
2022-07-31 20:56:21 +0200

    feat: Home Page with navigation toolbar added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 //material imports here
-import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+//custom components here.
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatSliderModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    //material module imports
+    MatToolbarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
